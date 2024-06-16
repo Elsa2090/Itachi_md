6d14dc4f5b631cefbdc85f280636d3db9663f510
Madara-tech <168005814+Elsa2090@users.noreply.github.com>
2024-06-16 02:12:29 +0000

    Update _updater.js

--- Commands/_updater.js
+++ Commands/_updater.js
@@ -22,7 +22,7 @@ cmd({
         async(Void, citel, text,{ isCreator }) => {
             if (!isCreator) return citel.reply('This command is only for my owner')
             let commits = await DB.syncgit()
-            if (commits.total === 0) { citel.reply(`Hey ${citel.pushName}. You have latest version installed.`)} 
+            if (commits.total === 0) { citel.reply(`Hey ${citel.pushName}. You have latest version Itachi_md installed.`)} 
             else {
                 let update = await DB.sync()
                 return await Void.sendMessage(citel.chat, { text: update,});
