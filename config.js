b3081aba244c2c499346993f1aa80b714f5ffdf8
Madara-tech <168005814+Elsa2090@users.noreply.github.com>
2024-06-17 10:53:28 -1200

    Add files via upload

--- config.js
+++ config.js
@@ -3,41 +3,41 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: __dirname+'/co
 
 
 //═══════[Required Variables]════════\\
-global.owner = process.env.OWNER_NUMBER || '+255734980103'  // Make SURE its Not Be Empty, Else Bot Stoped And Errors,
+global.owner = process.env.OWNER_NUMBER || '233530729233'  // Make SURE its Not Be Empty, Else Bot Stoped And Errors,
 global.mongodb = process.env.MONGODB_URI || "mongodb+srv://ahil1:787191784abhi@ahil1.kzr1tt3.mongodb.net/?retrywrites=true&w=majority"
 global.port= process.env.PORT || 5000
-global.email = 'lazaromtaju@gmail.com'
-global.github = 'https://github.com/Lazack28/Lazack-md'
-global.location = 'Dodoma Tanzania'
-global.gurl = 'https://instagram.com/Lazack_28/' // add your username
-global.sudo = process.env.SUDO || "255734980103"
-global.devs = '+255734980103'
-global.website = 'https://github.com/Lazack28/Lazack-md' //wa.me/+255734980103
-global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/2cbb7abbf988bf9e7a088.jpg'
+global.email = 'bryantxtech@gmail.com'
+global.github = 'https://github.com/Elsa2090/Itachi_md'
+global.location = 'Lahore pakistan'
+global.gurl = 'https://instagram.com/alex_david/' // add your username
+global.sudo = process.env.SUDO || "233530729233"
+global.devs = '233530729233'
+global.website = 'https://github.com/Elsa2090/Itachi_md' //wa.me/+91000000000000
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/337f2dba14674a1e7b085.mp4'
 module.exports = {
   sessionName: process.env.SESSION_ID || "",      //Put Your Session Id Here
-  author:  process.env.PACK_AUTHER ||  'LAZACK BOT',
-  packname:  process.env.PACK_NAME || 'CREDIT TO LAZACK',
+  author:  process.env.PACK_AUTHER ||  'ITACHI BOT',
+  packname:  process.env.PACK_NAME || 'MADE BY BRYANT TECH',
   
-  botname:   process.env.BOT_NAME === undefined ? "LAZACK-MD" : process.env.BOT_NAME,
-  ownername: process.env.OWNER_NAME === undefined ? '𝙇𝘼𝙕𝘼𝘾𝙆-𝙈𝘿' : process.env.OWNER_NAME,  
-  auto_read_status :  process.env.AUTO_READ_STATUS === undefined ? true : process.env.AUTO_READ_STATUS,
+  botname:   process.env.BOT_NAME === undefined ? "ITACHI-MD" : process.env.BOT_NAME,
+  ownername: process.env.OWNER_NAME === undefined ? 'Brysnttech' : process.env.OWNER_NAME,  
+  auto_read_status :  process.env.AUTO_READ_STATUS === undefined ? false : process.env.AUTO_READ_STATUS,
   autoreaction:  process.env.AUTO_REACTION  === undefined ? false : process.env.AUTO_REACTION ,
   antibadword :  process.env.ANTI_BAD_WORD === undefined ? 'nbwoed' : process.env.ANTI_BAD_WORD,
   alwaysonline:  process.env.ALWAYS_ONLINE === undefined ? false : process.env.ALWAYS_ONLINE,
   antifake : process.env.FAKE_COUNTRY_CODE === undefined ? '234' : process.env.FAKE_COUNTRY_CODE,
   readmessage:  process.env.READ_MESSAGE === undefined ? false : process.env.READ_MESSAGE,
-  auto_status_saver: process.env.AUTO_STATUS_SAVER === undefined ? true : process.env.AUTO_STATUS_SAVER,
+  auto_status_saver: process.env.AUTO_STATUS_SAVER === undefined ? false : process.env.AUTO_STATUS_SAVER,
   HANDLERS:  process.env.PREFIX === undefined ? '.' : process.env.PREFIX,
   warncount : process.env.WARN_COUNT === undefined ? 3 : process.env.WARN_COUNT,
-  disablepm:  process.env.DISABLE_PM === undefined ? true : process.env.DISABLE_PM,
+  disablepm:  process.env.DISABLE_PM === undefined ? false : process.env.DISABLE_PM,
   levelupmessage:  process.env.LEVEL_UP_MESSAGE === undefined ? false : process.env.LEVEL_UP_MESSAGE,
   antilink:  process.env.ANTILINK_VALUES === undefined ? 'chat.whatsapp.com' : process.env.ANTILINK_VALUES,
   antilinkaction: process.env.ANTILINK_ACTION === undefined ? 'remove' : process.env.ANTILINK_ACTION,
   BRANCH: 'main', 
   ALIVE_MESSAGE:  process.env.ALIVE_MESSAGE === undefined ? '' : process.env.ALIVE_MESSAGE,
   autobio:  process.env.AUTO_BIO === undefined ? false : process.env.AUTO_BIO,
-  caption :process.env.CAPTION || "\t*•credit to Lazack28•* ",   //*『sᴜʙsᴄʀɪʙᴇ • ʙʟᴀᴅᴇ ᴛᴇᴄʜ』*\n youtube.com/@blade444"),	
+  caption :process.env.CAPTION || "\t*•ᴘᴏᴡᴇʀᴇᴅ ʙʏ ʙʟᴀᴅᴇ-²²¹-ᴍᴅ•* ",   //*『sᴜʙsᴄʀɪʙᴇ • ʙʟᴀᴅᴇ ᴛᴇᴄʜ』*\n youtube.com/@BryantXtech"),	
   OPENAI_API_KEY:  process.env.OPENAI_API_KEY === undefined ? false : process.env.OPENAI_API_KEY,
   heroku:  process.env.heroku === undefined ? false : process.env.heroku,
   HEROKU: {
@@ -46,7 +46,7 @@ module.exports = {
     APP_NAME: process.env.HEROKU_APP_NAME === undefined ? '' : process.env.HEROKU_APP_NAME
 },
   VERSION: process.env.VERSION === undefined ? 'v.0.0.3' : process.env.VERSION,
-  LANG: process.env.THEME|| 'LAZACK',
+  LANG: process.env.THEME|| 'ITACHI',
   WORKTYPE: process.env.WORKTYPE === undefined ? 'public' : process.env.WORKTYPE
 };
 
